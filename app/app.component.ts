258a68d895dfc7527feea4aa3531cf8b4a73e510
mattcaswell <matthewcaswell253@Gmail.com>
2017-06-27 14:06:25 -0700

    set up initial files and folders

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+  <h1 style="color:blue">Note Pad!</h1>
+  </div>
+  `
+})
+
+  export class AppComponent {
+
+}
